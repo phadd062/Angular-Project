b30afa448abedf1bdbf30d79110c19a5248a001e
Peter Haddad <phadd062@uottawa.ca>
2020-12-27 13:20:20 -0500

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component'
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-server',
-    templateUrl: './server.component.html'
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
 })
-export class ServerComponent {
-    serverId = 10;
-    serverStatus = 'offline';
+export class ServerComponent{
+  serverId = 10;
+  serverStatus = 'offline';
 
-    getServerStatus(){
-        return this.serverStatus;
-    }
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
